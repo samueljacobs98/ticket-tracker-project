37f5afc69ac76e1427e70715768a614d9488ede2
Samuel Jacobs <samueljacobs@Samuels-MacBook-Pro.local>
2022-03-10 12:21:43 +0000

    added parallax scroll effect to background

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import bgLeft from "../../assets/svgs/bg-left.svg";
 import bgRight from "../../assets/svgs/bg-right.svg";
 import "./Background.scss";
 
 const Background = () => {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-      <img src={bgLeft} className="background__img background__img--left" />
-      <img src={bgRight} className="background__img background__img--right" />
+      <img
+        src={bgLeft}
+        className="background__img background__img--left"
+        style={{ transform: `translateX(${-offsetY * 0.01}%)` }}
+      />
+      <img
+        src={bgRight}
+        className="background__img background__img--right"
+        style={{ transform: `translateX(${offsetY * 0.01}%)` }}
+      />
     </>
   );
 };
